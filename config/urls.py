7d2f043dc2930f0bab03e64a523a32ea0d213d6c
Rubén Martínez Bermejo <ruben.mbermejo@bosonit.com>
2022-04-05 13:47:52 +0200

    Fix put, patch, delete, methods

--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from django.conf.urls import include
-
 from config import settings
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
+                  # path('dj-rest-auth/', include('dj_rest_auth.urls')),
                   path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
                   path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
                   path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
+from rest_framework import viewsets, mixins, permissions
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
+
 from .serializers import UserSerializer, UpdateUserSerializer, FullUserSerializer, CreateUserSerializer
 from .models import User
 from drf_spectacular.utils import extend_schema, extend_schema_view
@@ -14,9 +16,14 @@ from drf_spectacular.utils import extend_schema, extend_schema_view
     destroy=extend_schema(description='Delete a user.'),
     create=extend_schema(description='Create a new user.'),
 )
-class UserViewSet(ModelViewSet):
+class UserViewSet(mixins.ListModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  mixins.CreateModelMixin,
+                  GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
 
     serializer_classes_by_action = {
         'update': UpdateUserSerializer,
@@ -26,17 +33,18 @@ class UserViewSet(ModelViewSet):
         'get_user_by_username': FullUserSerializer,
     }
 
-    @action(methods=["get"], detail=False, url_path='(?P<username>[^/.]+)', url_name="user")
-    def get_user_by_username(self, request, username):
-        """Get user data by username."""
+    permission_classes = (permissions.AllowAny,)
 
+    def _allowed_methods(self):
+        return [m.upper() for m in self.http_method_names if hasattr(self, m)]
+
+    @action(methods=["get"], detail=True, url_path='(?P<username>[^/.]+)', url_name="user")
+    def get_user_by_username(self, request, username):
         user = get_object_or_404(User, username=username)
         serializer = self.get_serializer(user)
         return Response(serializer.data)
 
     @action(methods=["get"], detail=False, url_path='me', url_name="me")
     def get_current_user(self, request):
-        """Get currently logged user data."""
-
         serializer = self.get_serializer(request.user)
         return Response(serializer.data)
