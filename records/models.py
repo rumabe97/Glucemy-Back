f68a4c6af5e5cb8ee169230b05754e2ca485f656
Rubén Martínez Bermejo <ruben.mbermejo@bosonit.com>
2022-04-06 17:29:59 +0200

    Add relation foods-records

--- records/models.py
+++ records/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 
+from foods.models import Foods
 from users.models import User
 
 
@@ -9,11 +10,12 @@ class Records(models.Model):
     carbohydrates = models.FloatField(default=0)
     annotations = models.TextField(default="", max_length=255)
     date = models.DateTimeField(auto_now_add=True)
+    foods = models.ManyToManyField(Foods, related_name="records", related_query_name='record')
     user = models.ForeignKey(
         User,
         on_delete=CASCADE,
         related_name='records',
-        related_query_name='records'
+        related_query_name='record'
     )
 
     class Meta:
