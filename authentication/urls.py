79ef8ec053a07973a02500e85f32f936a5471d6d
Rubén Martínez Bermejo <ruben.mbermejo@bosonit.com>
2022-04-21 14:13:13 +0200

    Configure outlook login

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,12 @@
 from django.urls.conf import path
 from rest_framework_simplejwt import views as jwt_views
 
-from authentication.views import RegisterView, GoogleLogin
+from authentication.views import RegisterView, GoogleLogin, OutlookLogin
 
 urlpatterns = [
     path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
     path('social-login/google/', GoogleLogin.as_view(), name='google_login'),
+    path('social-login/outlook/', OutlookLogin.as_view(), name='outlook_login'),
 ]
