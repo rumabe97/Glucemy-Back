d6186024e02fe9a9cd7886fd7a2423059232a237
rumabe97 <rumabe97@gmail.com>
2023-10-26 16:24:27 +0200

    test

--- vercel/django.py
+++
@@ -1,7 +0,0 @@
-from django.core.management import execute_from_command_line
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config")
-
-if __name__ == "__main__":
-    execute_from_command_line()
