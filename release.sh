eb9e05be67b5180ee3c6ac50cbfd58ba6c61930f
rumabe97 <rumabe97@gmail.com>
2023-10-26 17:44:16 +0200

    test

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 # build_files.sh
 pip install -r requirements.txt
 
-python manage.py migrate
-python manage.py loaddata foods/fixtures/foods.json
+python3.9 manage.py migrate
+python3.9 manage.py loaddata foods/fixtures/foods.json
