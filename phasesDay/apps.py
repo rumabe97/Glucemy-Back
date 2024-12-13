ab5b8c1d2b0b2dc8c65cf06527d933037eaac305
rumabe97 <rumabe97@gmail.com>
2024-12-13 18:44:24 +0100

    add phasesday

--- phasesDay/apps.py
+++ phasesDay/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PhasesdayConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'phasesDay'
+
+    def ready(self):
+        call_command('loaddata', 'phasesDay.json', verbosity=0)
