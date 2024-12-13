02f18e24458eb4824693cf578dcbc1dcca2be90b
rumabe97 <rumabe97@gmail.com>
2024-12-13 18:49:43 +0100

    Fix

--- phasesDay/apps.py
+++ phasesDay/apps.py
@@ -1,10 +1,9 @@
 from django.apps import AppConfig
-
+from django.core.management import call_command
 
 class PhasesdayConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'phasesDay'
-
 
     def ready(self):
         call_command('loaddata', 'phasesDay.json', verbosity=0)
