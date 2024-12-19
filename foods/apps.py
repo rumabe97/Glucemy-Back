8389f88cc5dec5013ffc40c8c1b1489c63dca24d
rumabe97 <rumabe97@gmail.com>
2024-12-19 20:18:27 +0100

    Test command bulk

--- foods/apps.py
+++ foods/apps.py
@@ -1,10 +1,32 @@
 from django.apps import AppConfig
 from django.core.management import call_command
+import json
+import os
+
 
 class FoodsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'foods'
 
     def ready(self):
-        call_command('load_foods_in_chunks', 'foods.json', chunk_size=30)
+        self.load_food()
         # call_command('migrate')
+
+    def load_food(self):
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        fixtures_path = os.path.join(base_dir, 'fixtures', 'foods.json')
+        with open(fixtures_path, 'r') as file:
+            data = json.load(file)
+
+        foods = []
+        from foods.models import Foods
+        for item in data:
+            foods.append(Foods(
+                id=item['pk'],
+                name=item['fields'].get('name', 'Desconocido'),
+                usual_measure=item['fields'].get('usual_measure', 0.0),
+                hc_rations=item['fields'].get('hc_rations', 0.0),
+                glycemic_index=item['fields'].get('glycemic_index', 0),
+            ))
+
+        Foods.objects.bulk_create(foods)
