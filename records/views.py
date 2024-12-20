8db26dff68b2af2a44a8c823bdf0d92fcef21b7c
rumabe97 <rumabe97@gmail.com>
2024-12-20 17:09:46 +0100

    Fix statistics

--- records/views.py
+++ records/views.py
@@ -65,7 +65,7 @@ class RecordViewSet(DynamicSerializersMixin, DynamicPermissionsMixin, viewsets.M
 
         queryset = Records.objects.annotate(day=TruncDay('created_date')).values('day').annotate(
             totalBlood=Sum('blood_glucose'), totalCarbohydrates=RawSQL(
-                "SELECT SUM(unnest(carbohydrates)) FROM records WHERE id = %s",
+                "SELECT SUM(unnest(carbohydrates)) FROM records_records WHERE id = %s",
                 params=[f"Records.id"])).order_by('day').filter(
             created_date__range=[start_date, end_date], user=arg.user)
 
